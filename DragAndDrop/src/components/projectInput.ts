fc7740ea277c7efbfc35be190ed64cb4c9462032
Rafael Tavares <rafael_tcb@hotmail.com>
2020-06-22 17:06:01 -0300

    Drag and Drop project

---
+++ DragAndDrop/src/components/projectInput.ts
@@ -0,0 +1,60 @@
+import Component from './baseComponent';
+import { autobind } from '../decorators/autobind';
+import { projectState } from '../state/project';
+import { validate } from '../util/validation';
+
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        super('project-input', 'app', true, 'user-input');
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
+    }
+
+    protected configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    protected renderContent() {}
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable = { value: enteredTitle, required: true };
+        const descriptionValidatable = { value: enteredDescription, required: true, minLength: 5 };
+        const peopleValidatable = { value: +enteredPeople, required: true, min: 1, max: 5 };
+
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+            alert('Invalid input, please try again');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            projectState.addProject(title, description, people);
+            this.clearInputs();
+        }
+    }
+}
